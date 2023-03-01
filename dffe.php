9ba04bdad84aa1760730171d8b24f29b0d893a97
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-01 19:54:55 +0000

    Add files via upload

---
+++ dffe.php
@@ -0,0 +1,12 @@
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if ($_FILES['video']['error'] == 0) {
+			$temp = $_FILES['video']['tmp_name'];
+			$name = $_FILES['video']['name'];
+			move_uploaded_file($temp, "uploads/$name");
+			echo "File uploaded successfully.";
+		} else {
+			echo "Error uploading file.";
+		}
+	}
+	?>
